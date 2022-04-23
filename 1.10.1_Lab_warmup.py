cb0980f8452c7772b17dbee7b14a9aa5ef7654cf
Bek Johansson <thedev.bek@gmail.com>
2022-04-23 06:44:44 -0500

    first commit

---
+++ 1.10.1_Lab_warmup.py
@@ -0,0 +1,11 @@
+user_num = int(input('Enter integer:\n'))
+print('You entered:', user_num)
+square = user_num * user_num
+print(user_num, 'squared is', square)
+cube = user_num * user_num * user_num
+print('And', user_num , 'cubed is', cube ,'!!')
+user_num2 = int(input('Enter another integer:\n'))
+add = user_num + user_num2
+sub = user_num * user_num2
+print(user_num, '+', user_num2, 'is', add)
+print(user_num, '*', user_num2, 'is', sub)
