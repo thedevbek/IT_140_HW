7ddeac792fdd213cba1e356dce4803472e03feaa
Bek Johansson <thedev.bek@gmail.com>
2022-05-20 22:02:24 -0500

    updated

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 
-coins = ['dollars', 'quarters', 'dimes', 'nickels', 'pennies']
-coin_value = [100, 25, 10, 5, 1]
-user_total = 0
-for coin in coins:
-    number_of_coins = 0
-    while user_total > coin_value:
-        user_total -= coin_value # user_total = user_total - 100
-        number_of_coins += 1
-        print(user_total)
-        print(coins)
+# coins = ['dollars', 'quarters', 'dimes', 'nickels', 'pennies']
+# coin_value = [100, 25, 10, 5, 1]
+# user_total = 0
+# for coin in coins:
+#     number_of_coins = 0
+#     while user_total > coin_value:
+#         user_total -= coin_value # user_total = user_total - 100
+#         number_of_coins += 1
+#         print(user_total)
+#         print(coins)
+
+def main():
+    print('Hello, World!')
+
+if __name__ == '__main__':
+    main()
