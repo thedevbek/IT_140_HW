0670346b7713001b42c074b40857be6fd756930f
Bek Johansson <thedev.bek@gmail.com>
2022-05-20 22:00:41 -0500

    made guessing game for class

--- 1.21_divide_by_x.py
+++
@@ -1,6 +0,0 @@
-user_num = input()
-x = input()
-first_time = int(user_num) // int(x)
-second_time = int(first_time) // int(x)
-third_time = int(second_time) // int(x)
-print(first_time, second_time,third_time)
--- 1.23.1_Variables_input_type_conversion.py
+++
@@ -1,7 +0,0 @@
-user_int = int(input('Enter integer (32 - 126):\n'))
-
-# FIXME (1): Finish reading other items into variables, then output the four values on a single line separated by a space
-   
-# FIXME (2): Output the four values in reverse
-   
-# FIXME (3): Convert the integer to a character, and output that character
